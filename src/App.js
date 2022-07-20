bb53936b4a6702446759a92ddf24a8457a5c4432
rico4pepe <rico4pepe@gmail.com>
2022-07-20 14:21:57 +0100

    Seting up the css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-    <h1>Amazon</h1>
     {/** Header */}
-
+    <Header />
      {/** Home */}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import './Header.css'
+import amazonlogo from '../images/amazon-logo-transparent.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <img className='header_headerLogo' src={amazonlogo}  alt=''/>
+        <div className='header_headerSearch' >
+           <input className='header_headerInput' type = "text" />
+        </div>
+          <div className='header_headerNav'>
+                <div className='header_option'>
+                    <span className='header_optionLineOne'> Guest </span>
+                </div>
+                <div className='header_option'>
+                <span className='header_optionLineTwo'> Sign In</span>
+                </div>
+                <div className='header_option'>
+                <span className='header_optionLineThree'> Return Order</span>
+                </div>
+                <div className='header_option'>
+                <span className='header_optionLineFour'> Cart</span>
+                </div>
+          </div>
+    </div>
   )
 }
 
